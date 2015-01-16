e9b86a05c6e057fb46a1f975d3920d16a7e22bd7
Pete Herbst <pete@peteherbst.com>
2015-01-16 15:44:11 -0600

    Added additional code comments

--- geocode-addresses.php
+++ geocode-addresses.php
@@ -1,23 +1,34 @@
 <?php
   
+  // This is your Google Maps API key.
+  // You can setup and manage your keys on the Google Developers Console.
+  // https://console.developers.google.com/project
   $apiKey = "YOUR_KEY";
+  
   $addresses = array();
   
   require 'vendor/autoload.php';
   use GuzzleHttp\Client;
   
+  // Change the fopen value to the name of your CSV with addresses in the first column.
+  // Note that you should NOT include a column header.
   $file = fopen("to-geocode.csv", "r");
   while (($line = fgetcsv($file)) !== FALSE) {
     $addresses[] = $line[0];
   }
   fclose($file);
   
+  // Loop through all of the address values, formatting them and geocoding.
+  // We've added a second delay, using sleep(). Use usleep() if you want
+  // a shorter delay.
   foreach ($addresses as $address) {
     sleep(1);
     $formattedAddress = str_replace(" ", "+", $address);
     geocodeAddress($address, $formattedAddress);
   }
 
+  // Geocodes and prints the results. If you want an output file, just send
+  // the script to a TSV file using the command line.
   function geocodeAddress($address, $formattedAddress) {
     $client = new Client();
     $response = $client->get("https://maps.googleapis.com/maps/api/geocode/json?address=$formattedAddress&key=$apiKey", ['future' => true]);
