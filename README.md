1087dbb4d3ce52d00ce80aa6f88d0b326838ee45
Pete Herbst <pherbst@fieldmuseum.org>
2017-01-27 12:15:55 -0600

    Updated to Guzzle 6; fixed coding standards issues; updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # google-geocode-addresses
-A PHP script that uses Guzzle to geocode addresses using Google Maps API v3.
+A PHP script that uses Guzzle (6.0) to geocode addresses using Google Maps API v3.
+
+# Example Usage
+From command line: `php geocode-addresses.php addresses.tsv > geocoding.tsv`
--- geocode-addresses.php
+++ geocode-addresses.php
@@ -4,14 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-// This is your Google Maps API key.
-// You can setup and manage your keys on the Google Developers Console.
-// https://console.developers.google.com/project
-$apiKey = "YOUR_KEY";
-
 $addresses = array();
 
-// Open, read the CSV file and put into the addresses array.
+// Open, read the text file and put into the addresses array.
 $file = fopen($argv[1], "r");
 while (($line = fgetcsv($file)) !== FALSE) {
     $addresses[] = $line[0];
@@ -31,15 +26,22 @@ foreach ($addresses as $address) {
 // the script to a TSV file using the command line.
 function geocodeAddress($address, $formattedAddress) {
     $client = new Client();
+
+    // This is your Google Maps API key.
+    // You can setup and manage your keys on the Google Developers Console.
+    // https://console.developers.google.com/project
+    $apiKey = "API_KEY";
+
     $response = $client->get(
         "https://maps.googleapis.com/maps/api/geocode/json?address=$formattedAddress&key=$apiKey",
         ['future' => true]
     );
-    $json = $response->json();
+    $contents = $response->getBody()->getContents();
+    $json = json_decode($contents);
 
-    if (!empty($json['results'][0]['geometry']['location'])) {
-        $lat = $json['results'][0]['geometry']['location']['lat'];
-        $lng = $json['results'][0]['geometry']['location']['lng'];
+    if (!empty($json->results[0]->geometry->location)) {
+        $lat = $json->results[0]->geometry->location->lat;
+        $lng = $json->results[0]->geometry->location->lng;
         print $address . "\t" . $lat . "\t" . $lng . "\n";
     } else {
         print $address . "\t" . "No Lat" . "\t" . "No Lng" . "\n";
